81281303a880a927148ab28b7a4105d58dde6931
Chiho Seong <chseong@postech.ac.kr>
2024-10-11 11:21:49 +0000

    FIX: macro error

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -10,15 +10,15 @@
 
 typedef int32_t fp32;
 
-#define FP32_TO_FP (N)          N * FIXED_POINT_F
-#define FP32_TO_INT (F)         F / FIXED_POINT_F
-#define FP32_TO_INT_ROUND (F)   F >= 0 ? \
+#define FP32_TO_FP(N)          N * FIXED_POINT_F
+#define FP32_TO_INT(F)         F / FIXED_POINT_F
+#define FP32_TO_INT_ROUND(F)   F >= 0 ? \
     (F + FIXED_POINT_F / 2) / FIXED_POINT_F : (F - FIXED_POINT_F / 2) / FIXED_POINT_F
-#define FP32_FP32_ADD (A, B)    A + B
-#define FP32_FP32_SUB (A, B)    A - B
-#define FP32_INT_ADD (A, B)     A + FP32_TO_FP(B)
-#define FP32_INT_SUB (A, B)     A - FP32_TO_FP(B)
-#define FP32_FP32_MUL (A, B)    ((int64_t) A) * B / FIXED_POINT_F
-#define FP32_INT_MUL (A, B)     A * B
-#define FP32_FP32_DIV (A, B)    ((int64_t) A) * FIXED_POINT_F / B
-#define FP32_INT_DIV (A, B)     A / B
+#define FP32_FP32_ADD(A, B)    A + B
+#define FP32_FP32_SUB(A, B)    A - B
+#define FP32_INT_ADD(A, B)     A + FP32_TO_FP(B)
+#define FP32_INT_SUB(A, B)     A - FP32_TO_FP(B)
+#define FP32_FP32_MUL(A, B)    ((int64_t) A) * B / FIXED_POINT_F
+#define FP32_INT_MUL(A, B)     A * B
+#define FP32_FP32_DIV(A, B)    ((int64_t) A) * FIXED_POINT_F / B
+#define FP32_INT_DIV(A, B)     A / B

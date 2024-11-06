60a49bfff6b359efa100a13dba84b3cf3e83301b
Chiho Seong <chseong@postech.ac.kr>
2024-11-06 08:43:07 +0000

    ADD(assn2): temp impl about args passing

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -437,7 +437,8 @@ setup_stack (void **esp)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
-        *esp = PHYS_BASE;
+        //TODO: 핀토스 문서 추천의 임시 구현
+        *esp = PHYS_BASE - 12;
       else
         palloc_free_page (kpage);
     }

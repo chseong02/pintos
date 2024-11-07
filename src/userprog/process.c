f9eb1b446ba4a15cc7ea155ac3f2f140dc832a23
Chiho Seong <chseong@postech.ac.kr>
2024-11-07 10:26:02 +0000

    ADD(assn2): right user prog thread create name

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -38,8 +38,10 @@ process_execute (const char *file_name)
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
 
+  char *save_ptr;
+  char *real_file_name = strtok_r (fn_copy, " ", &save_ptr);
   /* Create a new thread to execute FILE_NAME. */
-  tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
+  tid = thread_create (real_file_name, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy); 
   return tid;

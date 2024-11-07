41720c084ea1a02f0eb531c739a7b3e049b33069
Kyungmin Kim <m4080m@postech.ac.kr>
2024-11-07 23:29:13 +0900

    ADD(assn2): syscall argument, exit, halt

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,8 +3,12 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
+static int get_user (const uint8_t *uaddr);
+static bool put_user (uint8_t *udst, uint8_t byte);
+
  	
 /* Reads a byte at user virtual address UADDR.
    UADDR must be below PHYS_BASE.
@@ -31,7 +35,7 @@ put_user (uint8_t *udst, uint8_t byte)
   return error_code != -1;
 }
 
-static bool
+bool
 check_ptr_in_user_space(const void *ptr)
 {
   return ptr < PHYS_BASE;
@@ -43,20 +47,53 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
+void
+get_args(int *sp, int *dst, int num)
+{
+  for(int i = 0; i < num; i++)
+  {
+    int *src = sp + num + 1;
+    if(check_ptr_in_user_space(src)) dst[i] = get_user(src);
+    else sys_exit(-1);
+  }
+}
+
 static void
 syscall_handler (struct intr_frame *f) 
 {
   printf ("system call!\n");
+  
+  int arg[4];
 
   //TODO: pointer 검증 필요할지도?
   switch(*(uint32_t *)(f->esp)) {
-    case SYS_EXIT:
     case SYS_HALT:
-      thread_exit();
+      sys_halt();
+      break;
+    case SYS_EXIT:
+      get_args(f->esp, arg, 1);
+      sys_exit(arg[0]);
+      break;
     default:
       ;
   }
   
   //TODO: 구현 완료시 exit 삭제
   //thread_exit ();
+}
+
+void
+sys_halt()
+{
+  shutdown_power_off();
+  NOT_REACHED();
+}
+
+void
+sys_exit(int exit_code)
+{
+  struct thread *cur = thread_current();
+  printf("%s: exit(%d)\n", cur->name, exit_code);
+  thread_exit();
+  NOT_REACHED();
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,15 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include "lib/stdint.h"
+#include "lib/stdbool.h"
+
+void get_args(int *sp, int *dst, int num);
+bool check_ptr_in_user_space(const void *ptr);
+
 void syscall_init (void);
+
+void sys_halt();
+void sys_exit(int exit_code);
 
 #endif /* userprog/syscall.h */

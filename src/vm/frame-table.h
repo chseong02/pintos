617e41712a8d55a54b8e3012cf0f49103361ae90
Chiho Seong <chseong@postech.ac.kr>
2024-11-29 07:22:43 +0000

    ADD(assn3): make file code about vm

--- src/vm/frame-table.h
+++ src/vm/frame-table.h
@@ -7,8 +7,6 @@ enum falloc_flags
 };
 
 void frame_table_init (void);
-void* falloc_get_frame_w_upage (enum falloc_flags flags, void* upage);
+void* falloc_get_frame_w_upage (enum falloc_flags flags, void *upage);
 void falloc_free_frame_from_upage (void *upage);
 void falloc_free_frame_from_frame (void *frame);
-struct frame_table_entry* find_frame_table_entry_from_upage (void *upage);
-struct frame_table_entry* find_frame_table_entry_from_frame (void *frame);

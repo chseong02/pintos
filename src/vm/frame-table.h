c95278c29a47755dd31a9e5275ca606721ae8c71
Chiho Seong <chseong@postech.ac.kr>
2024-11-29 05:50:09 +0000

    ADD(assn3): falloc flags

--- src/vm/frame-table.h
+++ src/vm/frame-table.h
@@ -1,3 +1,10 @@
+/* Flags for falloc_get_frame_w_upage */
+enum falloc_flags
+{
+    FAL_ASSERT = 001,  /* Panic When fail to allocate frame */
+    FAL_ZERO = 002,    /* Fill Frame with Zero */
+    FAL_USER = 004,    /* Get Frame from User pool */
+};
 
 void frame_table_init (void);
 void* falloc_get_frame_w_upage (enum falloc_flags, void* upage);

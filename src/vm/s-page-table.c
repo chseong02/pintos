4e0246b00770311033f360083431ce6ed17fdcf7
Chiho Seong <chseong@postech.ac.kr>
2024-11-29 08:51:39 +0000

    ADD(assn3): temp s page table add

--- src/vm/s-page-table.c
+++ src/vm/s-page-table.c
@@ -1,22 +1,29 @@
 #include "vm/s-page-table.h"
+#include "threads/malloc.h"
+#include "threads/thread.h"
 
 struct s_page_table_entry 
 {
 	bool present;
 	bool in_swap;
+    bool is_lazy;
 	bool has_loaded;
+
 	bool writable;
 	bool is_dirty;
 	bool is_accessed;
-	bool is_lazy;
+	
 	struct file* file;
 	off_t file_ofs;
 	uint32_t file_read_bytes;
 	uint32_t file_zero_bytes;
+    enum falloc_flags flags;
+
 	size_t swap_idx;
+
 	void *upage;
 	void *kpage;
-	enum falloc_flags flags;
+	
 	struct hash_elem elem;
 };
 
@@ -26,14 +33,14 @@ init_s_page_table (struct hash* s_page_table)
     hash_init (s_page_table, s_page_table_hash_func, s_page_table_hash_less_func, NULL);
 }
 
-unsigned
+static unsigned
 s_page_table_hash_func (const struct hash_elem *e, void *aux)
 {
 	struct s_page_table_entry *entry = hash_entry (e, struct s_page_table_entry, elem);
 	return hash_bytes (&entry->upage, 32);
 }
 
-bool
+static bool
 s_page_table_hash_less_func (const struct hash_elem *a, 
     const struct hash_elem *b, void *aux)
 {
@@ -42,3 +49,38 @@ s_page_table_hash_less_func (const struct hash_elem *a,
 	return _a->upage < _b->upage;
 }
 
+bool
+s_page_table_add (bool is_lazy, struct file *file, off_t file_ofs, 
+	bool writable, void *upage, void *kpage, uint32_t file_read_bytes, 
+	uint32_t file_zero_bytes, enum falloc_flags flags)
+{
+    struct s_page_table_entry *entry = malloc (sizeof *entry);
+    if (!entry)
+        return false;
+
+    // TODO: Check Is Exist upage 
+    // and return result
+    
+    struct thread *thread = thread_current ();
+    entry->present = true;
+    entry->in_swap = false;
+    entry->is_lazy = is_lazy;
+    entry->has_loaded = !is_lazy;
+	
+	entry->writable = writable;
+	entry->is_dirty = false;
+	entry->is_accessed = false;
+    
+	entry->file = file;
+	entry->file_ofs = file_ofs;
+	entry->file_read_bytes = file_read_bytes;
+	entry->file_zero_bytes = file_zero_bytes;
+	entry->flags = flags;
+	
+	entry->upage = upage;
+	entry->kpage = kpage;
+
+	hash_insert (&thread->s_page_table, &entry->elem);
+
+    return true;
+}

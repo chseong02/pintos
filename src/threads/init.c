d82a78402c5b7d77dac7ec1338d758e1afcd09c6
Chiho Seong <chseong@postech.ac.kr>
2024-11-29 07:35:21 +0000

    FIX(assn3): init frame table

--- src/threads/init.c
+++ src/threads/init.c
@@ -22,6 +22,7 @@
 #include "threads/palloc.h"
 #include "threads/pte.h"
 #include "threads/thread.h"
+#include "vm/frame-table.h"
 #ifdef USERPROG
 #include "userprog/process.h"
 #include "userprog/exception.h"
@@ -98,7 +99,7 @@ main (void)
   palloc_init (user_page_limit);
   malloc_init ();
   paging_init ();
-
+  frame_table_init();
   /* Segmentation. */
 #ifdef USERPROG
   tss_init ();

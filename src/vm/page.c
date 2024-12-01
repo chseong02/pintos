9e732676b7a86aba2311400aff2f2e2870b18663
Chiho Seong <chseong@postech.ac.kr>
2024-12-01 08:44:50 +0000

    ADD(assn3): find page fro uvaddr impl

--- src/vm/page.c
+++ src/vm/page.c
@@ -1,8 +1,15 @@
 #include "vm/page.h"
+#include "vm/s-page-table.h"
 
-void* find_page_from_uaddr (void *uaddr)
+void* find_page_from_uvaddr (void *uaddr)
 {
     // Not Implementated yet.
+    void *upage = (uint32_t) uaddr & 0xFFFFF000;
+    struct s_page_table_entry *entry = find_s_page_table_entry_from_upage (upage);
+    if (!entry)
+        return NULL;
+    if (entry->present)
+        return entry->upage;
     return NULL;
 }
 

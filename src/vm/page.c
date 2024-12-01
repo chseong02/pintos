5c0b5c3d377d4d21a76bb9e9402d68d53ddcffae
Chiho Seong <chseong@postech.ac.kr>
2024-12-01 08:35:46 +0000

    ADD: lazy loading related func interface

---
+++ src/vm/page.c
@@ -0,0 +1,13 @@
+#include "vm/page.h"
+
+void* find_page_from_uaddr (void *uaddr)
+{
+    // Not Implementated yet.
+    return NULL;
+}
+
+bool make_page_binded (void *upage)
+{
+    // Not Implementated yet.
+    return false;
+}
---
+++ src/vm/page.h
@@ -0,0 +1,4 @@
+#include <stdbool.h>
+
+void* find_page_from_uaddr (void* uaddr);
+bool make_page_binded (void* upage);

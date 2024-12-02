4958df3e73ada0db2a38d61494e48bb8ff03fa45
Chiho Seong <chseong@postech.ac.kr>
2024-12-02 06:34:48 +0000

    FIX(assn3): read boundary test case

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -125,54 +125,59 @@ kill (struct intr_frame *f)
 static void
 page_fault (struct intr_frame *f) 
 {
-  bool not_present;  /* True: not-present page, false: writing r/o page. */
-  bool write;        /* True: access was write, false: access was read. */
-  bool user;         /* True: access by user, false: access by kernel. */
-  void *fault_addr;  /* Fault address. */
+   bool not_present;  /* True: not-present page, false: writing r/o page. */
+   bool write;        /* True: access was write, false: access was read. */
+   bool user;         /* True: access by user, false: access by kernel. */
+   void *fault_addr;  /* Fault address. */
 
-  /* Obtain faulting address, the virtual address that was
-     accessed to cause the fault.  It may point to code or to
-     data.  It is not necessarily the address of the instruction
-     that caused the fault (that's f->eip).
-     See [IA32-v2a] "MOV--Move to/from Control Registers" and
-     [IA32-v3a] 5.15 "Interrupt 14--Page Fault Exception
-     (#PF)". */
-  asm ("movl %%cr2, %0" : "=r" (fault_addr));
+   /* Obtain faulting address, the virtual address that was
+      accessed to cause the fault.  It may point to code or to
+      data.  It is not necessarily the address of the instruction
+      that caused the fault (that's f->eip).
+      See [IA32-v2a] "MOV--Move to/from Control Registers" and
+      [IA32-v3a] 5.15 "Interrupt 14--Page Fault Exception
+      (#PF)". */
+   asm ("movl %%cr2, %0" : "=r" (fault_addr));
 
-  /* Turn interrupts back on (they were only off so that we could
-     be assured of reading CR2 before it changed). */
-  intr_enable ();
+   /* Turn interrupts back on (they were only off so that we could
+      be assured of reading CR2 before it changed). */
+   intr_enable ();
 
-  /* Count page faults. */
-  page_fault_cnt++;
+   /* Count page faults. */
+   page_fault_cnt++;
 
-  /* Determine cause. */
-  not_present = (f->error_code & PF_P) == 0;
-  write = (f->error_code & PF_W) != 0;
-  user = (f->error_code & PF_U) != 0;
+   /* Determine cause. */
+   not_present = (f->error_code & PF_P) == 0;
+   write = (f->error_code & PF_W) != 0;
+   user = (f->error_code & PF_U) != 0;
 
-  /* Kernel caused page fault by accessing user memory */
-  if(!user && check_ptr_in_user_space(fault_addr))
-  {
-   f->eip = (void *)f->eax;
-   f->eax = -1;
+   /* Kernel caused page fault by accessing user memory */
+   if(user && !check_ptr_in_user_space (fault_addr))
+      sys_exit(-1);
+   /* User caused page fault */
+   void* upage = find_page_from_uaddr (fault_addr);
+   if (!upage)
+      sys_exit (-1);
+   
+   bool success = make_page_binded (upage);
+   if (!success)
+      sys_exit (-1);
+   
+   // else if (!success)
+   // {
+   //    f->eip = (void *)f->eax;
+   //    f->eax = -1;
+   // }
+
    return;
-  }
-  /* User caused page fault */
-  void* upage = find_page_from_uaddr (fault_addr);
-  if (!upage)
-   sys_exit (-1);
-  if (!make_page_binded (upage))
-   sys_exit (-1);
-  return;
-  /* To implement virtual memory, delete the rest of the function
-     body, and replace it with code that brings in the page to
-     which fault_addr refers. */
-//   printf ("Page fault at %p: %s error %s page in %s context.\n",
-//           fault_addr,
-//           not_present ? "not present" : "rights violation",
-//           write ? "writing" : "reading",
-//           user ? "user" : "kernel");
-//   kill (f);
+   /* To implement virtual memory, delete the rest of the function
+      body, and replace it with code that brings in the page to
+      which fault_addr refers. */
+   //   printf ("Page fault at %p: %s error %s page in %s context.\n",
+   //           fault_addr,
+   //           not_present ? "not present" : "rights violation",
+   //           write ? "writing" : "reading",
+   //           user ? "user" : "kernel");
+   //   kill (f);
 }
 

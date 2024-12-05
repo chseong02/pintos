15304e097e2d6c1c0136e319372147b6fadc44ba
Chiho Seong <chseong@postech.ac.kr>
2024-12-05 10:31:29 +0000

    ADD(assn3); init swap table

--- src/vm/swap-table.c
+++ src/vm/swap-table.c
@@ -2,13 +2,31 @@
 #include <bitmap.h>
 #include "threads/synch.h"
 #include "vm/s-page-table.h"
+#include "devices/block.h"
+#include "threads/vaddr.h"
+#include "threads/malloc.h"
 
 struct swap_table
 {
+    struct block* swap_block;
     struct lock lock;
     struct bitmap *used_map;
 };
 
 static struct swap_table swap_table;
+
+void
+init_swap_table (void)
+{
+    swap_table.swap_block = block_get_role (BLOCK_SWAP);
+    if (!swap_table.swap_block)
+        PANIC ("Failed to get Swap Disk.");
+    uint32_t sector_count = block_size (swap_table.swap_block);
+    uint64_t page_count = ((uint64_t) sector_count) * BLOCK_SECTOR_SIZE / PGSIZE;
+    lock_init (&(swap_table.lock));
+    swap_table.used_map = bitmap_create (page_count);
+    if (!swap_table.used_map)
+        PANIC ("Failed to allocate swap disk bitmap.");
+}
 
 

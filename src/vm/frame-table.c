c02a1d2c6d76c592d565084cb935d4cb8951945b
Chiho Seong <chseong@postech.ac.kr>
2024-12-05 12:02:42 +0000

    ADD: detail at pick thread upage to swap

--- src/vm/frame-table.c
+++ src/vm/frame-table.c
@@ -8,6 +8,7 @@
 
 static struct list frame_table;
 static struct lock frame_table_lock;
+static struct frame_table_entry *clock_hand;
 
 struct frame_table_entry
 {
@@ -22,6 +23,7 @@ struct frame_table_entry
 void
 frame_table_init (void)
 {
+    clock_hand = NULL;
     list_init (&frame_table);
     lock_init (&frame_table_lock);
 }
@@ -120,6 +122,12 @@ falloc_free_frame_from_upage (void *upage)
         return;
     
     lock_acquire (&frame_table_lock);
+    if (clock_hand == entry)
+    {
+        clock_hand = list_next (entry);
+        if (clock_hand == list_end (&frame_table))
+            clock_hand = NULL;
+    }
     list_remove (&entry->elem);
     lock_release (&frame_table_lock);
     palloc_free_page (entry->kpage);
@@ -133,6 +141,12 @@ falloc_free_frame_from_frame (void *frame)
     if (!entry)
         return;
     lock_acquire (&frame_table_lock);
+    if (clock_hand == entry)
+    {
+        clock_hand = list_next (entry);
+        if (clock_hand == list_end (&frame_table))
+            clock_hand = NULL;
+    }
     list_remove (&entry->elem);
     lock_release (&frame_table_lock);
     palloc_free_page (entry->kpage);
@@ -144,10 +158,17 @@ pick_thread_upage_to_swap (struct thread *t, void* upage)
 {
     lock_acquire (&frame_table_lock);
     struct list_elem *e;
+
+    if (clock_hand == NULL)
+    {
+        clock_hand = list_begin (&frame_table);
+        if (clock_hand == list_end (&frame_table))
+            return false;
+    }
     //TODO: lock 관리 더 세밀하게.
     //TODO: 두바퀴를 돌아 circle queue처럼 보이게.
     //TODO: 시작 시점은 기록하고 그 시점부터 탐색 시작해야함.
-    for (e = list_begin (&frame_table); e != list_end (&frame_table);
+    for (e = clock_hand; e != list_end (&frame_table);
          e = list_next (e))
     {
         struct frame_table_entry *entry = 
@@ -180,7 +201,7 @@ pick_thread_upage_to_swap (struct thread *t, void* upage)
         pagedir_set_accessed (pd, entry->upage, false);
     }
     //TODO: 초침 직전까지 탐색하게.
-    for (e = list_begin (&frame_table); e != list_end (&frame_table);
+    for (e = list_begin (&frame_table); e != clock_hand;
          e = list_next (e))
     {
         struct frame_table_entry *entry = 
